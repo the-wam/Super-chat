7e506ce83a8d572f87d9d7e5170c6f8b04fd679f
loic <loicbanco@gmail.com>
2019-02-11 17:19:24 +0100

    ajout de la date a la connection

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var server = require('http').createServer(app);
 var io = require('socket.io').listen(server);
 var ent = require('ent'); // Permet de bloquer les caractères HTML (sécurité équivalente à htmlentities en PHP)
 var moment = require('moment');
-var date = moment(new Date()).format('dddd, MMMM Do YYYY, h:mm:ss');
+
 
 //Middlewares
 
@@ -25,19 +25,20 @@ app.get('/watcher', function (req, res) {
   res.sendfile(__dirname + '/views/watcher.html');
 });
 
-io.sockets.on('connection', function (socket, pseudo, date) {
+io.sockets.on('connection', function (socket, pseudo) {
     // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
     socket.on('nouveau_client', function(pseudo) {
         pseudo = ent.encode(pseudo);
+        var date = moment(new Date()).format('dddd D, hh:mm:ss a');
         socket.pseudo = pseudo;
-        socket.date = date;
-        socket.broadcast.emit('nouveau_client', pseudo, date);
+        socket.broadcast.emit('nouveau_client', {pseudo: pseudo, date: date});
+
     });
 
     // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
     socket.on('message', function (message) {
         message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message, date:date});
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
     });
 });
 
